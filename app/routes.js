4bed4ba8b7bc3db933407fbf836396a8214feb0b
CRACK <Sarah.CRACK@EDUCATION.GOV.UK>
2024-04-12 15:10:04 +0100

    finished chec-answers, added ineligible page and updated routes for villain selection

--- app/routes.js
+++ app/routes.js
@@ -3,7 +3,22 @@
 // https://prototype-kit.service.gov.uk/docs/create-routes
 //
 
-const govukPrototypeKit = require('govuk-prototype-kit')
-const router = govukPrototypeKit.requests.setupRouter()
+const govukPrototypeKit = require("govuk-prototype-kit");
+const router = govukPrototypeKit.requests.setupRouter();
 
 // Add your routes here
+
+// Run this code when a form is submitted to 'superhero-power-answer'
+router.post("/superhero-power-answer", function (req, res) {
+  // Make a variable and give it the value from 'how-many-balls'
+  var superheroPower = req.session.data["superhero-power"];
+
+  // Check whether the variable matches a condition
+  if (superheroPower === "I'm a Villain") {
+    // Send user to ineligible page
+    res.redirect("/ineligible");
+  } else {
+    // Send user to check-answers page
+    res.redirect("/check-answers");
+  }
+});
